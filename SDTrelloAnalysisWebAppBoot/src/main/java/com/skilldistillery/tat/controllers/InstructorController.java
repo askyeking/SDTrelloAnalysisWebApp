014e8a0b57ade9dda97f7b2f98c5c5b58e48ac94
Anthony King <anthonyking@SDInstructor1.local>
2019-11-21 16:04:29 -0700

    TopicController methods stubbed

--- SDTrelloAnalysisWebAppBoot/src/main/java/com/skilldistillery/tat/controllers/InstructorController.java
+++ SDTrelloAnalysisWebAppBoot/src/main/java/com/skilldistillery/tat/controllers/InstructorController.java
@@ -10,6 +10,19 @@ import org.springframework.web.bind.annotation.RestController;
 import com.skilldistillery.tat.entities.Instructor;
 import com.skilldistillery.tat.services.InstructorService;
 
+
+/*
+ * keyword = keyword in topic name OR description
+ * xGET | /api/topics 															| Get all topics 
+ * xGET | /api/topics/{topic.id} 												| Get topic by ID 
+ * xGET | /api/topics/keyword/{keyword} 											| Get all topics by keyword 
+ * GET | /api/topics/startDate/{startDate}/endDate/{endDate} 					| Get all topics by start/end date 
+ * GET | /api/topics/keyword/{keyword}/startDate/{startDate}/endDate/{endDate} 	| Get all topics by keyword + date 
+ * GET | /api/topics/instructor/{instructor} 									| Get all topics by Instructor 
+ * GET | /api/topics/instructor/{instructor}/startDate/{startDate}/endDate/{endDate} 					| Get all topics by Instructor + Date 
+ * GET | /api/topics/instructor/{instructor}/keyword/{keyword} 											| Get all topics by Instructor + Keyword 
+ * GET | /api/topics/instructor/{instructor}/keyword/{keyword}/startDate/{startDate}/endDate/{endDate} 	| Get topics by Instructor, Keyword, Date
+ */
 @RestController
 @RequestMapping(path = "api")
 public class InstructorController {
@@ -27,4 +40,8 @@ public class InstructorController {
 	public List<Instructor> index() {
 		return instructorService.index();
 	}
+	
+	
+	
+	
 }
