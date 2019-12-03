d2f391c0ce600a7e5df6f6670209605a764b9e9b
Anthony King <anthonyking@SDInstructor1.local>
2019-12-03 11:27:09 -0700

    moved dependency for apache commons csv parser, and stubbed out CSVParserClient

--- SDTrelloAnalysisWebAppBoot/src/main/java/com/skilldistillery/tat/controllers/TopicController.java
+++ SDTrelloAnalysisWebAppBoot/src/main/java/com/skilldistillery/tat/controllers/TopicController.java
@@ -20,72 +20,75 @@ public class TopicController {
 	private TopicService topicSvc;
 
 	/*
-	 * keyword = keyword in topic name
-	 * -GET | /api/topics 															| Get all topics 
-	 * -GET | /api/topics/{topic.id} 												| Get topic by ID 
-	 * -GET | /api/topics/keyword/{keyword} 											| Get all topics by keyword 
-	 * -GET | /api/topics/startDate/{startDate}/endDate/{endDate} 					| Get all topics by start/end date 
-	 * -GET | /api/topics/keyword/{keyword}/startDate/{startDate}/endDate/{endDate} 	| Get all topics by keyword + date 
-	 * -GET | /api/topics/instructor/{instructor} 									| Get all topics by Instructor 
-	 * -GET | /api/topics/instructor/{instructor}/startDate/{startDate}/endDate/{endDate} 					| Get all topics by Instructor + Date 
-	 * -GET | /api/topics/instructor/{instructor}/keyword/{keyword} 											| Get all topics by Instructor + Keyword 
-	 * GET | /api/topics/instructor/{instructor}/keyword/{keyword}/startDate/{startDate}/endDate/{endDate} 	| Get topics by Instructor, Keyword, Date
+	 * keyword = keyword in topic name -GET | /api/topics | Get all topics -GET |
+	 * /api/topics/{topic.id} | Get topic by ID -GET | /api/topics/keyword/{keyword}
+	 * | Get all topics by keyword -GET |
+	 * /api/topics/startDate/{startDate}/endDate/{endDate} | Get all topics by
+	 * start/end date -GET |
+	 * /api/topics/keyword/{keyword}/startDate/{startDate}/endDate/{endDate} | Get
+	 * all topics by keyword + date -GET | /api/topics/instructor/{instructor} | Get
+	 * all topics by Instructor -GET |
+	 * /api/topics/instructor/{instructor}/startDate/{startDate}/endDate/{endDate} |
+	 * Get all topics by Instructor + Date -GET |
+	 * /api/topics/instructor/{instructor}/keyword/{keyword} | Get all topics by
+	 * Instructor + Keyword GET |
+	 * /api/topics/instructor/{instructor}/keyword/{keyword}/startDate/{startDate}/
+	 * endDate/{endDate} | Get topics by Instructor, Keyword, Date
 	 */
 
-	
 	@GetMapping("/")
 	public List<Topic> index() {
 		return topicSvc.findAll();
 	}
-	
+
 	@GetMapping("/{id}")
 	public Topic getTopicById(@PathVariable int id) {
-	return topicSvc.findById(id);
+		return topicSvc.findById(id);
 	}
-	
-	
+
 	@GetMapping("/keyword/{keyword}")
 	public List<Topic> findTopicsByKeyword(@PathVariable String keyword) {
-		
+
 		return null;
 	}
-	
-	
+
 	@GetMapping("/startDate/{startDate}/endDate/{endDate}")
 	public List<Topic> findTopicsInDateRange(@PathVariable Date startDate, @PathVariable Date endDate) {
-		
+
 		return null;
-		
+
 	}
-	
+
 	@GetMapping("/keyword/{keyword}/startDate/{startDate}/endDate/{endDate}")
-	public List<Topic> findTopicsByKeywordInDateRange(@PathVariable String keyword, @PathVariable Date startDate, @PathVariable Date endDate) {
-		
+	public List<Topic> findTopicsByKeywordInDateRange(@PathVariable String keyword, @PathVariable Date startDate,
+			@PathVariable Date endDate) {
+
 		return null;
 	}
-	
+
 	@GetMapping("/instructor/{instructor}")
 	public List<Topic> findTopicsByInstructor(@PathVariable Instructor instructor) {
-		
-		
+
 		return null;
 	}
-	
+
 	@GetMapping("instructor/{instructor}/startDate/{startDate}/endDate/{endDate}")
-	public List<Topic> findTopicsByInstructorInDateRange(@PathVariable Instructor instructor, @PathVariable Date startDate, @PathVariable Date endDate){
+	public List<Topic> findTopicsByInstructorInDateRange(@PathVariable Instructor instructor,
+			@PathVariable Date startDate, @PathVariable Date endDate) {
 		return null;
 	}
-	
+
 	@GetMapping("instructor/{instructor}/keyword/{keyword}")
-	public List<Topic> findTopicsByInstructorAndTopicKeyword(@PathVariable Instructor instructor, @PathVariable String keyword){
-		
+	public List<Topic> findTopicsByInstructorAndTopicKeyword(@PathVariable Instructor instructor,
+			@PathVariable String keyword) {
+
 		return null;
 	}
-	
+
 	@GetMapping("instructor/{instructor}/keyword/{keyword}/startDate/{startDate}/endDate/{endDate}")
 	public List<Topic> findTopicsByInstructorAndTopicKeywordInDateRange(@PathVariable Instructor instructor,
-			@PathVariable String keyword, @PathVariable Date startDate, @PathVariable Date endDate){
+			@PathVariable String keyword, @PathVariable Date startDate, @PathVariable Date endDate) {
 		return null;
 	}
-	
+
 }
