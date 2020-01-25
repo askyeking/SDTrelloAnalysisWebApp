8457209219aa97501897ad9568a1e8c18048e358
Emily Lau <emilylau@Brucebs-MacBook-Pro-2.local>
2020-01-25 10:13:52 -0700

    added SD24_Unit1 csv file

--- SDTrelloAnalysisWebAppBoot/src/main/java/com/skilldistillery/tat/controllers/CSVController.java
+++ SDTrelloAnalysisWebAppBoot/src/main/java/com/skilldistillery/tat/controllers/CSVController.java
@@ -10,10 +10,8 @@ public class CSVController {
 	ProcessBuilder processBuilder = new ProcessBuilder();
 
 	
-	
+	//this method will allow user to upload a CSV file to AWS.
 	public Boolean csvFileInProcessing() {
-		
-		
 //		processBuilder.command("bash", "")
 		
 		Process process;
