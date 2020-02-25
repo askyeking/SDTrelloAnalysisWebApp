22dad1dfe3e00144df64915514b03b9d9dec4c81
Anthony King <anthonyking@SDInstructor1.local>
2020-02-25 10:02:45 -0700

    topics and csvfiles persisting to database with string data for dates

--- SDTrelloAnalysisWebAppBoot/src/main/java/com/skilldistillery/tat/services/CSVParserServiceImpl.java
+++ SDTrelloAnalysisWebAppBoot/src/main/java/com/skilldistillery/tat/services/CSVParserServiceImpl.java
@@ -11,6 +11,10 @@ import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
@@ -20,15 +24,20 @@ import com.skilldistillery.tat.entities.Topic;
 import com.skilldistillery.tat.entities.TrelloCsvFile;
 
 public class CSVParserServiceImpl implements CSVParserService {
-
+	
+	EntityManagerFactory emf = Persistence.createEntityManagerFactory("tatpu");
+	EntityManager em = emf.createEntityManager();
+	
+	
 	public static void main(String[] args) {
 		CSVParserServiceImpl csvService = new CSVParserServiceImpl();
-		csvService.parseDateString("Day 1 - Wed 11/6");
-		csvService.createTrelloCSVFile("SD24_Unit1.csv");
+//		csvService.createTrelloCSVFile("SD24_Unit1.csv");
+		System.out.println(csvService.createTrelloCSVFile("SD24_Unit1.csv"));
 	}
 
 	public TrelloCsvFile createTrelloCSVFile(String fileName) {
 		TrelloCsvFile csv = new TrelloCsvFile();
+		csv.setFileName(fileName);
 		// hardcoded filepath value will be replaced with aws csv folder location for
 		// production
 		csv.setPathAndFileName("src/main/resources/static/" + fileName);
@@ -41,6 +50,10 @@ public class CSVParserServiceImpl implements CSVParserService {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		em.getTransaction().begin();
+		em.persist(csv);
+		em.flush();
+		em.getTransaction().commit();
 		return csv;
 	}
 
@@ -65,25 +78,26 @@ public class CSVParserServiceImpl implements CSVParserService {
 				Matcher matcher = datePattern.matcher(csvRecord.get("Card Name"));
 				// startAndEndDate list index 0 is start date 1 is end date
 				List<String> startAndEndDate = new ArrayList<>();
-				String yearBegin = "";
-				String yearEnd = "";
+
 				while (matcher.find()) {
 					String matchedDate = matcher.group();
 					startAndEndDate.add(matchedDate);
-					//TODO convert date string in startAndEndDate list
-					//     then set them in the csvfile passed into this method
-					
-					
-					
 					System.out.println(matchedDate);
-					
 				}
+				trelloFile.setStartDateString(startAndEndDate.get(0));
+				trelloFile.setEndDateString(startAndEndDate.get(1));
+
+				// TODO convert date string in startAndEndDate list
+				// then set them in the csvfile passed into this method
+				// WRITE EIGHT CASE SWITCH STATEMENT FOR DIFFERENT DATE FORMATS
 				continue;
 			}
 			// Accessing values by the names assigned to each column
 			String topicName = csvRecord.get("Card Name");
 			String instructors = csvRecord.get("Labels");
 			String[] instructorArray = instructors.split("\\s\\([^\\)]+\\)[,]?");
+			String description = csvRecord.get("Card Description");
+
 			// generate instructors for the topic
 			List<String> instructorStrings = Arrays.asList(instructorArray);
 			List<Instructor> instructorList = new ArrayList<>();
@@ -92,9 +106,29 @@ public class CSVParserServiceImpl implements CSVParserService {
 				Instructor instructor = new Instructor(instructorName);
 				instructorList.add(instructor);
 			}
-			LocalDate dateLectured = parseDateString(csvRecord.get("List Name"));
 
-			Topic topic = new Topic(topicName, instructorList, dateLectured, trelloFile);
+			// Get column name from trello, split into date and day of program
+			String dateAndDayLectured = csvRecord.get("List Name");
+			int beginIndex = dateAndDayLectured.lastIndexOf("Day ");
+			int endIndex = dateAndDayLectured.lastIndexOf("-");
+			String dateLecturedString = "";
+			String dayOfProgram = "";
+			if (beginIndex == 0) {
+				dayOfProgram = dateAndDayLectured.substring(beginIndex, endIndex);
+				dateLecturedString = parseDateStringAsString(dateAndDayLectured);
+			}
+			Topic topic = new Topic(topicName, instructorList, dateLecturedString, trelloFile);
+			topic.setDayOfProgram(dayOfProgram);
+			String boardName = csvRecord.get("Board Name");
+			beginIndex = boardName.lastIndexOf("SD");
+			String cohort = boardName.substring(beginIndex);
+			topic.setCohort(cohort);
+			
+			beginIndex = boardName.lastIndexOf("Unit");
+			String unit = boardName.substring(beginIndex, beginIndex + 5);
+			topic.setUnit(unit);
+			
+			
 			parsedTopics.add(topic);
 		}
 		return parsedTopics;
@@ -108,12 +142,24 @@ public class CSVParserServiceImpl implements CSVParserService {
 		String monthDayString = "";
 		LocalDate monthDay = null;
 		if (matcher.find()) {
-		monthDayString = matcher.group();
+			monthDayString = matcher.group();
 		}
 		System.out.println(monthDayString);
-		
-		//TODO Convert String into LocalDate before returning
+		// TODO Convert String into LocalDate before returning
 		return monthDay;
+	}
+
+	public String parseDateStringAsString(String dateToParse) {
+		// EXAMPLE DATA FROM CSV: "Day 1 - Wed 11/6"
+		// TALK TO ROB ABOUT START/END DATE CARD
+		Pattern datePattern = Pattern.compile("\\d{1,2}\\/\\d{1,2}");
+		Matcher matcher = datePattern.matcher(dateToParse);
+		String monthDayString = "";
+		if (matcher.find()) {
+			monthDayString = matcher.group();
+		}
+		// TODO Convert String into LocalDate before returning
+		return monthDayString;
 	}
 
 }
--- SDTrelloAnalysisWebAppJPA/src/main/java/com/skilldistillery/tat/entities/TrelloCsvFile.java
+++ SDTrelloAnalysisWebAppJPA/src/main/java/com/skilldistillery/tat/entities/TrelloCsvFile.java
@@ -3,6 +3,7 @@ package com.skilldistillery.tat.entities;
 import java.util.Date;
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -11,7 +12,10 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
+import org.hibernate.annotations.Cascade;
+
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.google.protobuf.DescriptorProtos.FieldDescriptorProto.Type;
 
 @Entity
 @Table(name="trello_csv_file")
@@ -33,8 +37,15 @@ public class TrelloCsvFile {
 	@Column(name = "end_date")
 	private Date endDate;
 	
+	@Column(name = "start_date_string")
+	private String startDateString;
+	
+	@Column(name = "end_date_string")
+	private String endDateString;
+	
+	
 	@JsonIgnore
-	@OneToMany(mappedBy="csvFile")
+	@OneToMany(mappedBy="csvFile", cascade = CascadeType.PERSIST)
 	List<Topic> topics;
 
 	public List<Topic> getTopics() {
@@ -88,11 +99,28 @@ public class TrelloCsvFile {
 	public void setEndDate(Date endDate) {
 		this.endDate = endDate;
 	}
+	
+	public String getStartDateString() {
+		return startDateString;
+	}
+
+	public void setStartDateString(String startDateString) {
+		this.startDateString = startDateString;
+	}
+
+	public String getEndDateString() {
+		return endDateString;
+	}
+
+	public void setEndDateString(String endDateString) {
+		this.endDateString = endDateString;
+	}
 
 	@Override
 	public String toString() {
 		return "TrelloCsvFile [id=" + id + ", fileName=" + fileName + ", pathAndFileName=" + pathAndFileName
-				+ ", startDate=" + startDate + ", endDate=" + endDate + "]";
+				+ ", startDateString=" + startDateString + ", endDateString=" + endDateString + ", topics=" + topics
+				+ "]";
 	}
 
 	@Override
@@ -100,10 +128,13 @@ public class TrelloCsvFile {
 		final int prime = 31;
 		int result = 1;
 		result = prime * result + ((endDate == null) ? 0 : endDate.hashCode());
+		result = prime * result + ((endDateString == null) ? 0 : endDateString.hashCode());
 		result = prime * result + ((fileName == null) ? 0 : fileName.hashCode());
 		result = prime * result + id;
 		result = prime * result + ((pathAndFileName == null) ? 0 : pathAndFileName.hashCode());
 		result = prime * result + ((startDate == null) ? 0 : startDate.hashCode());
+		result = prime * result + ((startDateString == null) ? 0 : startDateString.hashCode());
+		result = prime * result + ((topics == null) ? 0 : topics.hashCode());
 		return result;
 	}
 
@@ -120,6 +151,11 @@ public class TrelloCsvFile {
 			if (other.endDate != null)
 				return false;
 		} else if (!endDate.equals(other.endDate))
+			return false;
+		if (endDateString == null) {
+			if (other.endDateString != null)
+				return false;
+		} else if (!endDateString.equals(other.endDateString))
 			return false;
 		if (fileName == null) {
 			if (other.fileName != null)
@@ -138,8 +174,22 @@ public class TrelloCsvFile {
 				return false;
 		} else if (!startDate.equals(other.startDate))
 			return false;
+		if (startDateString == null) {
+			if (other.startDateString != null)
+				return false;
+		} else if (!startDateString.equals(other.startDateString))
+			return false;
+		if (topics == null) {
+			if (other.topics != null)
+				return false;
+		} else if (!topics.equals(other.topics))
+			return false;
 		return true;
 	}
+	
+	
+
+	
 	
 	
 	
